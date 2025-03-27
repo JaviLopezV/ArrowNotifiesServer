5efade57825b8a0f0a01831fb3e22e9f7a972908
JaviLopezV <69149519+JaviLopezV@users.noreply.github.com>
2025-03-27 09:38:46 +0100

    no database needed

--- db.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const mysql = require("mysql2/promise");
-
-// 📌 Conectar usando DATABASE_URL de Railway
-const pool = mysql.createPool(process.env.DATABASE_URL);
-
-module.exports = pool;
--- index.js
+++ index.js
@@ -2,33 +2,26 @@ const express = require("express");
 const webpush = require("web-push");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const pool = require("./db");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  })
+);
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.get("/users", async (req, res) => {
-  try {
-    const [rows] = await pool.query("SELECT * FROM users");
-    res.json(rows);
-  } catch (error) {
-    console.error("Error en MySQL:", error);
-    res.status(500).json({ error: "Error al conectar con la base de datos" });
-  }
-});
-
-const VAPID_KEYS = {
-  publicKey:
-    "BI39-j-0GtG0e1iZy8IZIwyqfHluHBD9Rkzdvy7yqBpnwAXyFBN4u8z6Wj3TL5GQpbhsedV2S6jHi-Y1Q9HSZMM",
-  privateKey: "-fLtSbyba-RrIEUlzotxlaMWChKTeeqvMNEcnFHF67o",
-};
+// const VAPID_KEYS = {
+//   publicKey:
+//     "BI39-j-0GtG0e1iZy8IZIwyqfHluHBD9Rkzdvy7yqBpnwAXyFBN4u8z6Wj3TL5GQpbhsedV2S6jHi-Y1Q9HSZMM",
+//   privateKey: "-fLtSbyba-RrIEUlzotxlaMWChKTeeqvMNEcnFHF67o",
+// };
 
 webpush.setVapidDetails(
   "mailto:jlopvil@gmail.com",
-  VAPID_KEYS.publicKey,
-  VAPID_KEYS.privateKey
+  process.env.VAPID_PUBLIC_KEY,
+  process.env.VAPID_PRIVATE_KEY
 );
 
 let subscriptions = [];
@@ -53,5 +46,5 @@ app.post("/sendNotification", (req, res) => {
 
   res.status(200).json({ message: "Notificación enviada" });
 });
-
-app.listen(5000, () => console.log("Servidor corriendo en el puerto 5000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
